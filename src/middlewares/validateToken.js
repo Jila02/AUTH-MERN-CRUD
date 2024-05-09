dd22935b9ed95a8955996faf7681e9a3c52a1989
Jila02 <issaclandin@gmail.com>
2024-05-08 23:52:15 -0500

    se agrego tasks routes y se termino de configurar el validatetoken.js

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -5,15 +5,17 @@ import { TOKEN_SECRET } from '../config.js';
 export const authRequired = (req,res,next)=>{
     const {token}= req.cookies;
     
-    if(!token) return res.tatus(401).json({
+    if(!token) 
+        return res.status(401).json({
         message:"No token, authorization denied"
     })
 
-    jwt.verify(token,TOKEN_SECRET,(err,decode)=>{
+    jwt.verify(token,TOKEN_SECRET,(err,user)=>{
         if(err) return res.status(403).json({
             message:"Invalid Token"
         })
+        req.user=user
+        next();
     })
 
-    next();
 }
