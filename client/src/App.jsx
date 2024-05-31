6d82e33855a48597b8af4bde5e3367f15e66a65f
Jila02 <issaclandin@gmail.com>
2024-05-31 01:22:15 -0500

    se agrego contexto al front y se realizo el manejo de errores

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-function app(){
-  return(
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<h1>Home page</h1>} />
-      <Route path='/login' element={<h1>login</h1>} />
-      <Route path='/register' element={<h1>register</h1>} />
-      <Route path='/tasks' element={<h1>tasks page</h1>} />
-      <Route path='/add-task' element={<h1>new tasks</h1>} />
-      <Route path='/tasks/:id' element={<h1>update task</h1>} />
-      <Route path='/profile' element={<h1>page profile</h1>} />
+import { AuthProvider } from "./context/AuthContext"
 
-    </Routes>
+import  LoginPage from './pages/LoginPage' 
+import RegisterPage from "./pages/registerPage"
+function App(){
+  return(
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<h1>Home page</h1>} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage/>} />
+          <Route path='/tasks' element={<h1>tasks page</h1>} />
+          <Route path='/add-task' element={<h1>new tasks</h1>} />
+          <Route path='/tasks/:id' element={<h1>update task</h1>} />
+          <Route path='/profile' element={<h1>page profile</h1>} />
+        </Routes>
      </BrowserRouter>
-  )
+    </AuthProvider> 
+  );
 }
 
-export default app
+export default App;
---
+++ client/src/pages/registerPage.jsx
@@ -0,0 +1,53 @@
+import {useForm} from 'react-hook-form'
+import { useAuth } from '../context/AuthContext'
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+function RegisterPage(){
+   const {register,handleSubmit,formState:{errors}} =useForm()
+   const {signup, isAuthenticated, errors: registerErrors}= useAuth();
+   const navigate= useNavigate()
+    useEffect(()=>{
+        if (isAuthenticated) navigate("/tasks")
+    },[isAuthenticated])
+
+    const onSubmit= handleSubmit(async (values)=>{
+        signup(values);
+    });
+
+    return(
+        <div className="bg-zinc-800 max-w-md p-10 rounded-md">
+            {
+                registerErrors.map((error,i)=>(
+                    <div className='bg-red-500 p-2 text-white' key={i}>
+                        {error}
+                    </div>
+                ))
+            }
+            <form onSubmit={onSubmit}>
+                <input type="text"  {...register("username",{required: true})} 
+                className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+                placeholder='username'/>
+                {
+                    errors.username &&( <p className="text-red-500">username is required</p>
+                )}
+
+                <input type="email" {...register("email",{required: true})}
+                className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+                placeholder='email'/>      
+                {
+                    errors.email &&( <p className="text-red-500">email is required</p>
+                )}
+
+                <input type="password" {...register("password",{required: true})} 
+                className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+                placeholder='password'/>
+                <button type="submit">register</button>
+                {
+                    errors.password &&( <p className="text-red-500">password is required</p>
+                )}
+            </form>
+        </div>
+    )    
+}   
+
+export default RegisterPage
